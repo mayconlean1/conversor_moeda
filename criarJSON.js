7559d0a7cc553f8e7df70ca7eb9b7ecb3924bee5
Maycon Leandrini <59230121+MayconLean@users.noreply.github.com>
2020-09-08 16:48:45 -0300

    build
    script para buscar o banco de dados na internet

---
+++ criarJSON.js
@@ -0,0 +1,41 @@
+const puppeteer = require('puppeteer'); 
+const fs = require('fs'); 
+
+const robo = async () =>{
+    const browser = await puppeteer.launch({headless:true});
+    const page = await browser.newPage();
+    const url = `https://pt.wikipedia.org/wiki/ISO_4217`
+    await page.goto(url);
+    //await page.screenshot({path: 'example.png'});
+    var paises =  await page.evaluate(()=>{
+      function isNumber(numero){
+        let tnumero = String(Number(numero)) == numero
+        return tnumero
+      }
+      let tempPais = {}
+      let baseRoot =  document.querySelector('.wikitable.sortable.jquery-tablesorter').children[1].children
+      for(tr in baseRoot){
+        if(isNumber(tr)){                      
+          let tempSigla = baseRoot[tr].children[0].textContent
+          let tempMoeda = baseRoot[tr].children[3].textContent
+          let imgBandeira = ''
+          let baseLocais = baseRoot[tr].children[4].children
+                                            
+          for(l in baseLocais){
+            if(isNumber(l)){                           
+              if (baseLocais[l].children[0] == undefined){
+                tempPais[baseLocais[l].textContent] = {'moeda': tempMoeda , 'sigla': tempSigla , 'bandeira': imgBandeira}
+                imgBandeira = ''
+              }else{
+                imgBandeira = baseLocais[l].children[0].src
+              }            
+            }
+          } 
+        } 
+      }
+      return tempPais                          
+    })
+    fs.writeFileSync('moeda_paises.json' , JSON.stringify(paises) )
+    await browser.close();
+  }
+robo()
